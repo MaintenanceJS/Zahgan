904217d567ad816bbeddc9759cec451e10208699
AmjadHasnawi <civilamjad227@gmail.com>
2018-12-13 15:31:43 +0200

    finish tests

--- react-client/src/App.test.js
+++ react-client/src/App.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Signup from './components/Signup'
-import Signin from './components/Signin'
+import Signup from './components/UserSignIn/Signup'
+import Signin from './components/UserSignIn/Signin'
 // import About from './components/About'
 import SignInCreator from './components/Creator/SigninCreator'
 //import EventClassNew from './components/EventClassNew'
-import SimpleMap from './components/map'
+import SimpleMap from './components/Creator/map'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { mount, shallow, render } from 'enzyme';
+configure({ adapter: new Adapter() });
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -48,6 +53,51 @@ it('renders SimpleMap without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<SimpleMap />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('Testing the Signup component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Signup />, div);
+  });
+  it('should render correctly in "debug" mode', () => {
+	 	// In debug mode, Chainer checks values of variables on runtime and shows more detailed error messages.
+	 	// It helps you to debug your programs. However, it requires some additional overhead time.
+    const component = shallow(<Signup debug />);
+    expect(component).toMatchSnapshot();
+  });
+  it('should render correctly with no props', () => {
+    const component = shallow(<Signup/>);
+    expect(component).toMatchSnapshot();
+  });
+  it('should render banner text correctly with given strings', () => {
+    const strings = ['one', 'two'];
+    const component = shallow(<Signup list={strings} />);
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe('Testing the Signin component', () => {
+	it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Signin />, div);
+  });
+	it('should render correctly in "debug" mode', () => {
+	 	// In debug mode, Chainer checks values of variables on runtime and shows more detailed error messages.
+	 	// It helps you to debug your programs. However, it requires some additional overhead time.
+    const component = shallow(<Signin debug />);
+  
+    expect(component).toMatchSnapshot();
+  });
+  it('should render correctly with no props', () => {
+    const component = shallow(<Signin/>);
+    expect(component).toMatchSnapshot();
+  });
+  it('should render banner text correctly with given strings', () => {
+    const strings = ['one', 'two'];
+    const component = shallow(<Signin list={strings} />);
+    expect(component).toMatchSnapshot();
+  });
 });
 
 
--- test/databasetest.js
+++ test/databasetest.js
@@ -1,40 +1,48 @@
 const assert = require('assert');
-const Student = require('../database-mongo/index');
-
+const db = require('../database-mongo/index.js');
+var request = require('supertest');
+var mocha = require('mocha');
 
 describe('save',function(){
-  var char;
+  var event;
+  
   //save to database to see it is saving or not
-	it('save to database',function(done){
-    	char = new Student.Student({
-     		 Name: 'Yahye',
-      		HomeWork:'Javascript',
+	it('save to database',function(){
+    	event = new db.Event({
+			email:'sdsada' ,
+			creatorName:'sdfsfr',
+			eventName: 'dffrrg',
+			cost:12,
+			des: 'sdfsf',
+			url: 'sdfsdf',
+			date:'fdfsfdg',
+			availableSeats: 342243,
+			eventLocation: [31, 35],
+			attending:[2],
+			imgName: 'sgrg'
     	});
 
-    	char.save().then(function(){
-      	assert(!char.isNew);
-      	done();
+    	event.save().then(function(){
+      	assert(event.isNew === false);
     	});
 
 	});
 
 //delete one from the database to check if you can remove specific one
-	it('Delete specific one from the database', function(done){
-    	Student.Student.findOneAndRemove({Name: 'Yahye'}).then(function(){
-      		Student.Student.findOne({Name: 'Yahye'}).then(function(result){
+	it('Delete specific one from the database', function(){
+    	db.Event.findOneAndRemove({email:'sdsada'}).then(function(){
+			db.Event.findOne({email:'sdsada'}).then(function(result){
         		assert(result === null);
-        		done();
       		});
     	});
   	});
 
   //update one 
 
-  it('Updates the saved data', function(done){
-      Student.Student.findOneAndUpdate({Name: 'Farah'}, {Name: 'None'}).then(function(){
-          Student.Student.findOne({_id: char._id}).then(function(result){
-              assert(result.Name === 'None');
-              done();
+  it('Updates the saved data', function(){
+      db.Event.findOneAndUpdate({email:'sdsada'}, {email: 'None'}).then(function(){
+		db.Event.findOne({_id: event._id}).then(function(result){
+              assert(result.email === 'None');
           });
       });
   });
@@ -42,3 +50,49 @@ describe('save',function(){
 
 
 });
+
+describe('Server Test', function () {
+
+    describe('Connection Test', function () {
+        it('Should have a response from the server ', function (done) {
+            request('http://127.0.0.1:3000').get('/').expect(200, done)
+        })
+        it('should resived error from the server with wrong path ', function (done) {
+            request('http://127.0.0.1:3000').get('/wrong').expect(404, done)
+        });
+    });
+});
+
+describe('POST', function () {
+
+    it('should register users ', function (done) {
+        request('http://127.0.0.1:3000').post('/signup').expect(200).send({
+            name: 'dana',
+            major: 'hhhhh',
+             email: 'dana@gfgf.com',
+            username: 'dd',
+            password: '3333',
+            description:'wow',
+            phoneNumber: '072852',
+        }).end(function (err, res) {
+            done()
+
+        })
+    })
+})
+describe('POST', function () {
+
+    it('users login', function (done) {
+        request("http://127.0.0.1:3000")
+          .post("/login")
+          .expect(200)
+          .send({
+            username: "dana",
+            password: "1232"
+          })
+          .end(function(err, res) {
+            done();
+          });
+    });
+});
+
